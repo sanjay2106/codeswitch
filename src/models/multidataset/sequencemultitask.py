7fb44d7b530cd2e229e5b742ad06267ca5298a17
sanjay2106 <125800945+sanjay2106@users.noreply.github.com>
2024-08-19 22:26:36 +0530

    Update sequencemultitask.py

--- src/models/multidataset/sequencemultitask.py
+++ src/models/multidataset/sequencemultitask.py
@@ -1,9 +1,9 @@
 from src.modules.base_model import BaseModel
-from torchcrf import CRF
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
-from torchmetrics.functional import accuracy, precision, recall, f1_score
+from torchcrf import CRF
+from torchmetrics.functional import precision, recall, f1_score
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -52,6 +52,7 @@ class SequenceMultiTaskModel(pl.LightningModule):
         
         self.log_vars = []
 
+        # Architecture: base model -> BiLSTM -> CRF
         self.bi_lstm = nn.LSTM(
             input_size=self.baseModel.config.hidden_size,
             hidden_size=256,
@@ -82,6 +83,85 @@ class SequenceMultiTaskModel(pl.LightningModule):
             self.add_module(f"Task {task_name} TaskHead", self.task_heads[i])
             self.register_parameter(name=f"Loss param {task_name}", param=self.log_vars[i])
 
+    def forward(self, base_model_outs):
+        base_outs = base_model_outs.last_hidden_state
+        lstm_outs, _ = self.bi_lstm(base_outs)
+        linear_outs = self.linear(lstm_outs)
+
+        return linear_outs
+
+    def training_step(self, batch, batch_idx):
+        input_ids, attention_mask, labels, task_no = batch
+        
+        linear_outs = self(self.baseModel(input_ids, attention_mask))
+        
+        outlist = [ [[], [], []] for x in range(len(self.label2ids)) ]        
+        for x in range(len(linear_outs)):
+            task_id = task_no[x]
+            outlist[task_id][0].append(linear_outs[x])
+            outlist[task_id][1].append(attention_mask[x])
+            outlist[task_id][2].append(labels[x])
+        
+        loss = 0
+        for task_id in range(len(outlist)):
+            if len(outlist[task_id][0]) == 0:
+                continue
+            
+            outlist[task_id] = [ torch.stack(y).to(device) for y in outlist[task_id] ]
+            emissions, attention_mask, labels = outlist[task_id]
+            outlist[task_id] = self.task_heads[task_id](emissions, labels, attention_mask)
+            task_loss, task_path = outlist[task_id]
+            loss += torch.exp(-self.log_vars[task_id]) * task_loss + self.log_vars[task_id]
+            
+            metrics = self._compute_metrics(task_path, labels, f'{self.task_names[task_id]} train', task_id)
+            self.log_dict(metrics, on_step=False, on_epoch=True)
+
+        self.log("loss/train", loss, on_step=False, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        input_ids, attention_mask, labels, task_no = batch
+        
+        linear_outs = self(self.baseModel(input_ids, attention_mask))
+        
+        outlist = [ [[], [], []] for x in range(len(self.label2ids)) ]        
+        for x in range(len(linear_outs)):
+            task_id = task_no[x]
+            outlist[task_id][0].append(linear_outs[x])
+            outlist[task_id][1].append(attention_mask[x])
+            outlist[task_id][2].append(labels[x])
+        
+        loss = 0
+        for task_id in range(len(outlist)):
+            if len(outlist[task_id][0]) == 0:
+                continue
+            
+            outlist[task_id] = [ torch.stack(y).to(device) for y in outlist[task_id] ]
+            emissions, attention_mask, labels = outlist[task_id]
+            outlist[task_id] = self.task_heads[task_id](emissions, labels, attention_mask)
+            task_loss, task_path = outlist[task_id]
+            loss += task_loss
+            
+            metrics = self._compute_metrics(task_path, labels, f'{self.task_names[task_id]} val', task_id)
+            self.log_dict(metrics, on_step=False, on_epoch=True)
+
+        self.log("loss/val", loss, on_step=False, on_epoch=True)
+        return loss
+
+    def _compute_metrics(self, preds: torch.Tensor, labels: torch.Tensor, mode: str, task_id: int):
+
+        preds = torch.reshape(preds, (-1, 1))
+        preds = preds.type_as(labels)
+
+        labels = torch.reshape(labels, (-1, 1))
+        
+        metrics = {}
+        metrics[f"prec/{mode}"] = precision(preds, labels, num_classes=len(self.label2ids[task_id]) + 1, ignore_index=self.special_tag_ids[task_id], average="macro")
+        metrics[f"rec/{mode}"] = recall(preds, labels, num_classes=len(self.label2ids[task_id]) + 1, ignore_index=self.special_tag_ids[task_id], average="macro")
+        metrics[f"f1/{mode}"] = f1_score(preds, labels, num_classes=len(self.label2ids[task_id]) + 1, ignore_index=self.special_tag_ids[task_id], average="macro")
+
+        return metrics
+    
     def configure_optimizers(self):
         parameters = [
             {'params': self.baseModel.parameters()},
@@ -106,3 +186,4 @@ class SequenceMultiTaskModel(pl.LightningModule):
         )
         
         return optimizer
+
