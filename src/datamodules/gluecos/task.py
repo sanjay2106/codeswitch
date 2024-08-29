ca5d91736a18d9018c22f5fe5fd1fc1b2b26c5b6
sanjay2106 <125800945+sanjay2106@users.noreply.github.com>
2024-08-29 11:00:42 +0530

    Update task.py

--- src/datamodules/gluecos/task.py
+++ src/datamodules/gluecos/task.py
@@ -1,6 +1,7 @@
 class Task:
-    def __init__(self, label2id, name, train_path, val_path):
+    def __init__(self, label2id, name, train_path, val_path,task_no=None):
         self.label2id = label2id
         self.name = name
         self.train_path = train_path
         self.val_path = val_path
+        self.task_no = task_no
